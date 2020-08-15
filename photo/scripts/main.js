eea208d7ca70d1d4f5554b9289690134e8256f1c
kylerpan <kylerpan691@gmail.com>
2020-08-14 21:10:44 -0700

    preview images for photo

--- photo/scripts/main.js
+++ photo/scripts/main.js
@@ -1,10 +1,20 @@
-// let images = document.getElementById(img);
+var images = document.getElementsByTagName('img');
+var feature_image = images[0];
+var preview = document.getElementById("preview-window")
 
-// images.onclick = function() {
-//     let mySrc = myImage.getAttribute('src');
-//     if(mySrc === 'images/firefox-icon.png'){
-//         myImage.setAttribute('src', 'github.png');
-//     } else {
-//         myImage.setAttribute('src', 'images/firefox-icon.png');
-//     }
-// }
+for (let i in images){
+    let image = images[i];
+    if (image == feature_image)
+        continue;
+    image.onclick = function() {
+        console.log("clicked");
+        let img_src = image.getAttribute('src');
+        console.log(img_src);
+        feature_image.setAttribute('src', img_src);
+        preview.style.display = "grid";
+    }
+}
+
+preview.onclick = function() {
+    preview.style.display = "none";
+}
