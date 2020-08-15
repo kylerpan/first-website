9a1cbd0243ed83b23e61c0df84ba06d6e60937f3
kylerpan <kylerpan691@gmail.com>
2020-08-15 01:14:07 -0700

    finished preview images

--- photo/scripts/main.js
+++ photo/scripts/main.js
@@ -1,20 +1,48 @@
-var images = document.getElementsByTagName('img');
-var feature_image = images[0];
-var preview = document.getElementById("preview-window")
+/* for desktop preview */
+var content_desktop = document.getElementById('content-desktop');
+var desktop_images = content_desktop.getElementsByTagName('img');
+var desktop_feature_image = desktop_images[0];
+var desktop_preview = document.getElementById("preview-window-d")
 
-for (let i in images){
-    let image = images[i];
-    if (image == feature_image)
+for (let i in desktop_images){
+    let image = desktop_images[i];
+    console.log(image);
+    if (image == desktop_feature_image)
         continue;
     image.onclick = function() {
         console.log("clicked");
         let img_src = image.getAttribute('src');
         console.log(img_src);
-        feature_image.setAttribute('src', img_src);
-        preview.style.display = "grid";
+        desktop_feature_image.setAttribute('src', img_src);
+        desktop_preview.style.display = "grid";
     }
 }
 
-preview.onclick = function() {
-    preview.style.display = "none";
+desktop_preview.onclick = function() {
+    desktop_preview.style.display = "none";
+}
+
+
+/* for mobile preview */
+var content_mobile = document.getElementById('content-mobile');
+var mobile_images = content_mobile.getElementsByTagName('img');
+var mobile_feature_image = mobile_images[0];
+var mobile_preview = document.getElementById("preview-window-m")
+
+for (let i in mobile_images){
+    let image = mobile_images[i];
+    console.log(image);
+    if (image == mobile_feature_image)
+        continue;
+    image.onclick = function() {
+        console.log("clicked");
+        let img_src = image.getAttribute('src');
+        console.log(img_src);
+        mobile_feature_image.setAttribute('src', img_src);
+        mobile_preview.style.display = "grid";
+    }
+}
+
+mobile_preview.onclick = function() {
+    mobile_preview.style.display = "none";
 }
